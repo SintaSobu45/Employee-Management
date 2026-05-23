ddfff12bc519d894db1a6b053da4c3a4ec680a43
SintaSobu45 <sintaaitrich33@gmail.com>
2026-05-23 15:08:06 +0530

    Fixed

--- Program.cs
+++ Program.cs
@@ -44,8 +44,8 @@ namespace EmployeeMVC
 
             app.MapControllerRoute(
                 name: "default",
-                //pattern: "{controller=Account}/{action=Login}/{id?}");
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Login}/{id?}");
+            //pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
